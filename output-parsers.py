495699d200e5a02b1a8810f23a66839d5b19102a
David Nowak <david.nowak@consort-group.com>
2025-01-07 11:49:00 +0100

    Add new output parsers

--- output-parsers.py
+++ output-parsers.py
@@ -4,7 +4,7 @@ load_dotenv()
 from langchain_openai import ChatOpenAI 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser, CommaSeparatedListOutputParser, JsonOutputParser
-# from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 
 
 # Instantiate Model
@@ -30,47 +30,75 @@ def call_string_output_parser():
     return chain.invoke({"input": "panda"})
 
 
+def call_list_output_parser():
+    # Prompt Template
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Génère une liste de 10 synonymes du mot suivant. Retourne le résultat sous forme de liste, avec des virgules comme séparateurs."),
+            ("human", "{input}")
+        ]
+    )
 
-# def call_list_output_parser():
-#     # Prompt Template
-#     prompt = ChatPromptTemplate.from_messages(
-#         [
-#             ("system", "Génère une liste de 10 synonymes du mot suivant. Retourne le résultat sous forme de liste, avec des virgules comme séparateurs."),
-#             ("human", "{input}")
-#         ]
-#     )
+    parser = CommaSeparatedListOutputParser()
 
-#     parser = CommaSeparatedListOutputParser()
+    # Create LLM Chain
+    chain = prompt | llm | parser
 
-#     # Create LLM Chain
-#     chain = prompt | llm | parser
-
-#     return chain.invoke({"input": "triste"})
-
-
-# def call_json_output_parser():
-#     # Prompt Template
-#     prompt = ChatPromptTemplate.from_messages(
-#         [
-#             ("system", "Extrais de l'information de la phrase suivante. \nInstructions de format: {format_instructions}"),
-#             ("human", "{phrase}")
-#         ]
-#     )
-
-#     class Person(BaseModel):
-#         name: str = Field(description="Le nom de la personne.")
-#         age: int = Field(description="L'âge de la personne.")
+    return chain.invoke({"input": "triste"})
 
 
-#     parser = JsonOutputParser(pydantic_object=Person)
+def call_json_output_parser():
+    # Prompt Template
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Extrais de l'information de la phrase suivante. \nInstructions de format: {format_instructions}"),
+            ("human", "{phrase}")
+        ]
+    )
 
-#     # Create LLM Chain
-#     chain = prompt | llm | parser
-
-#     return chain.invoke({"phrase": "Max est un garçon qui a 30 ans."})
-
+    class Person(BaseModel):
+        name: str = Field(description="Le nom de la personne.")
+        age: int = Field(description="L'âge de la personne.")
 
 
-print(call_string_output_parser())
+    parser = JsonOutputParser(pydantic_object=Person)
+
+    # Create LLM Chain
+    chain = prompt | llm | parser
+
+    return chain.invoke({
+        "phrase": "Max est un garçon qui a 30 ans.",
+        "format_instructions": parser.get_format_instructions()
+    })
+
+def call_json_output_parser_recipe():
+    # Prompt Template
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Extrais de l'information de la phrase suivante. \nInstructions de format: {format_instructions}"),
+            ("human", "{phrase}")
+        ]
+    )
+
+    class Recipe(BaseModel):
+        recette: str = Field(description="Le nom de la recette.")
+        ingredients: list = Field(description="Le nom de l'ingrédient.")
+        alcoolPresence: bool = Field(description="Présence ou non d'alcool dans la recette.")
+
+
+    parser = JsonOutputParser(pydantic_object=Recipe)
+
+    # Create LLM Chain
+    chain = prompt | llm | parser
+
+    return chain.invoke({
+        "phrase": "Les ingrédients pour une Pizza Margherita sont des oignons, des tomates, de la pâte à pizza et de la crême fraîche.",
+        "format_instructions": parser.get_format_instructions()
+    })
+
+
+
+# print(call_string_output_parser())
 # print(call_list_output_parser())
 # print(call_json_output_parser())
+print(call_json_output_parser_recipe())
